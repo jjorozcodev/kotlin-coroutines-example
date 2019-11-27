f4d7e9cfc780a536742505ecada193ef3163f1f3
JJ <jjorozcodev@gmail.com>
2019-11-27 16:42:12 -0600

    Implemented Coroutines
    Created two method to simulate two tasks using coroutines and applying its libraries and dependencies.

--- CoroutinesExample/app/src/main/java/com/orozcoder/jj/coroutines_example/MainActivity.kt
+++ CoroutinesExample/app/src/main/java/com/orozcoder/jj/coroutines_example/MainActivity.kt
@@ -2,11 +2,39 @@ package com.orozcoder.jj.coroutines_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT = "Method Result!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnClick.setOnClickListener{
+            // IO, Main, Default
+            CoroutineScope(Default).launch {
+                fakeApiRequest()
+            }
+        }
+    }
+
+    private suspend fun fakeApiRequest(){
+        val resultVar = getFirstResult()
+    }
+
+    private suspend fun getFirstResult() : String{
+        logThread("getFirstResult")
+        delay(1000)
+        return RESULT
+    }
+
+    private fun logThread(methodName: String){
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
     }
 }
