256569d1303a78e0d7d057e0e1143d5a58f8559f
JJ <jjorozcodev@gmail.com>
2019-11-29 12:18:37 -0600

    Solved error with threads and coroutines.
    Using Main thread to show changes into UI. And the IO thread for request simulation to network.

--- CoroutinesExample/app/src/main/java/com/orozcoder/jj/coroutines_example/MainActivity.kt
+++ CoroutinesExample/app/src/main/java/com/orozcoder/jj/coroutines_example/MainActivity.kt
@@ -6,8 +6,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,10 +27,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setNewText(input: String){
+        val newText = txtMsg.text.toString() + "\n$input"
+        txtMsg.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
     private suspend fun fakeApiRequest(){
         val resultVar = getFirstResult()
         println("debug: ${resultVar}")
-        txtMsg.setText(resultVar)
+        //txtMsg.setText(resultVar)
+        setTextOnMainThread(resultVar)
     }
 
     private suspend fun getFirstResult() : String{
